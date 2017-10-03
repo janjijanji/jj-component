365a30f5cdb120201bef2872b3c9c5f14beee787
raymondralibi <ralibi@starqle.com>
2017-10-03 14:00:51 +0700

    Add global helper methods related to verdicts

--- src/index.js
+++ src/index.js
@@ -6,5 +6,19 @@ module.exports = function install (Vue) {
   }
   install.installed = true
 
+  Vue.prototype.$getJjVerdictKeys = () => ["NOT_RATED", "BROKEN", "STUCK", "COMPROMISE", "IN_PROGRESS", "KEPT"]
+  const indexOfVerdict = verdict => Vue.prototype.$getJjVerdictKeys().indexOf(verdict)
+
+  Vue.prototype.$getJjVerdictLabel = verdict => ["Not rated", "Broken", "Stuck", "Compromise", "In progress", "Kept"][indexOfVerdict(verdict)]
+  Vue.prototype.$getJjVerdictColor = (verdict, opacity) => {
+    const index = indexOfVerdict(verdict)
+    const colors = [[170,170,170], [170,0,0], [229,175,42], [237,237,68], [52,170,207], [98,133,25]]
+    return "rgba(" + colors[index][0] + ", " + colors[index][1] + ", " + colors[index][2] + ", " + opacity + ")"
+  }
+
+  Vue.prototype.$getJjVerdictKeyByIndex = index => Vue.prototype.$getJjVerdictKeys()[index]
+  Vue.prototype.$getJjVerdictLabelByIndex = index => Vue.prototype.$getJjVerdictLabel(Vue.prototype.$getJjVerdictKeyByIndex(index))
+  Vue.prototype.$getJjVerdictColorByIndex = (index, opacity) => Vue.prototype.$getJjVerdictColor(Vue.prototype.getJjVerdictKeyByIndex(index), opacity)
+
   Vue.component(JjLogo.name, JjLogo)
 }
